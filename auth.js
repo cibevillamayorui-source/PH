d1fef3251564f646ba3f83d654f124083e6bf992
cibevillamayorui-source <cibe.villamayor.ui@phinmaed.com>
2026-04-23 16:32:29 +0800

    auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,20 @@
-const USERS={
- admin:{username:"admin",password:"1234"},
- staff:{username:"staff",password:"1234"}
+const USERS = {
+  admin: {username:"admin", password:"1234"},
+  staff: {username:"staff", password:"1234"}
 };
 
 function login(role){
- if(username.value===USERS[role].username && password.value===USERS[role].password){
-  localStorage.setItem("role",role);
-  location.href=role==="admin"?"admin-dashboard.html":"staff-dashboard.html";
- } else alert("Invalid login");
+
+  if(username.value === USERS[role].username &&
+     password.value === USERS[role].password){
+
+    localStorage.setItem("role", role);
+
+    location.href = role === "admin"
+      ? "admin-dashboard.html"
+      : "staff-dashboard.html";
+
+  } else {
+    alert("Invalid login");
+  }
 }
